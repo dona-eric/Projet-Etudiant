4a1036bcf59594ba60cf542e51a66a7632e2e476
dona-eric <donaerickoulodji@gmail.com>
2024-12-09 14:38:15 +0100

    suppression des espces du code

--- Data_Health_Student/Student_app/app_health.py
+++ Data_Health_Student/Student_app/app_health.py
@@ -5,21 +5,25 @@ from Data_python import heure_cours_transformer
 import sys
 
 
-sys.path.append('/home/dona-erick/Projet-Etudiant') 
+sys.path.append("/home/dona-erick/Projet-Etudiant")
 
 # laod the model
-model_etudiant = joblib.load('/home/dona-erick/Projet-Etudiant/Data_Health_Student/Analyse_health_student/model_etudiant.pkl', mmap_mode='r')
+model_etudiant = joblib.load(
+    "/home/dona-erick/Projet-Etudiant/Data_Health_Student/Analyse_health_student/model_etudiant.pkl",
+    mmap_mode="r",
+)
 
 app = FastAPI()
+
 
 @app.get("/hello")
 def get_mesage():
     try:
         content = f"You're welcome in StudentApp"
-        return content 
+        return content
     except Exception as e:
         return f"Error {str(e)}"
-    
+
 
 @app.post("/risque_level_student")
 def risque_etudiant(data: dict):
@@ -30,14 +34,19 @@ def risque_etudiant(data: dict):
         predictions = model_etudiant.predict(df)
         prediction = predictions[0]
         if prediction == 2:
-           return {"niveau_risque": "High", "message": "Le niveau de risque de l'étudiant est élevé."}
+            return {
+                "niveau_risque": "High",
+                "message": "Le niveau de risque de l'étudiant est élevé.",
+            }
         elif prediction == 1:
-            return {"niveau_risque": "Medium", "message": "Le niveau de risque de l'étudiant est moyen."}
+            return {
+                "niveau_risque": "Medium",
+                "message": "Le niveau de risque de l'étudiant est moyen.",
+            }
         else:
-            return {"niveau_risque": "Low", "message": "Le niveau de risque de l'étudiant est faible."}
+            return {
+                "niveau_risque": "Low",
+                "message": "Le niveau de risque de l'étudiant est faible.",
+            }
     except Exception as e:
         return HTTPException(status_code=500, detail={str(e)})
-    
-        
-        
-        
